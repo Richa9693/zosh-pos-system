4bf1289cafa116c96396b4a91f419496ce77409e
RICHA RANI <richarani906@gmail.com>
2026-01-17 22:46:51 +0530

    Added JWT provider, constants, validator and custom user implementation

---
+++ src/main/java/com/zosh/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.zosh.configuration;
+
+public class JwtConstant {
+    public static final String JWT_SECRET = "difherifhfrrfhrufhuierfpiuefyergfreyfehygfyerbtxoide8dcdhdfreofgekjnpisdhncoertfprefedf7cergyurdhuidh";
+    public static final String JWT_HEADER = "Authorization";
+}

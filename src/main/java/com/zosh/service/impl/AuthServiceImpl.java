db5d25ac0fc24cd2aa598658e533690ff899984d
RICHA RANI <richarani906@gmail.com>
2026-01-20 23:28:05 +0530

    Add UserMapper and update AuthService implementation

--- src/main/java/com/zosh/service/impl/AuthServiceImpl.java
+++ src/main/java/com/zosh/service/impl/AuthServiceImpl.java
@@ -3,14 +3,17 @@ package com.zosh.service.impl;
 import com.zosh.configuration.JwtProvider;
 import com.zosh.domain.UserRole;
 import com.zosh.exceptions.UserException;
+import com.zosh.mapper.UserMapper;
 import com.zosh.modal.User;
 import com.zosh.payload.dto.UserDto;
 import com.zosh.payload.response.AuthResponse;
 import com.zosh.repository.UserRepository;
 import com.zosh.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.mbeans.UserMBean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -46,15 +49,30 @@ public class AuthServiceImpl implements AuthService {
 
         newUser.setUpdatedAt(LocalDateTime.now());
 
-        userRepository.save(newUser);
+        User savedUser = userRepository.save(newUser);
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(userDto.getEmail(), userDto.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Registered Successfully");
+        authResponse.setUser(UserMapper.toDto(savedUser));
 
         return null;
     }
 
     @Override
     public AuthResponse login(UserDto userDto) {
+       String email = userDto.getEmail() ;
+       String password = userDto.getPassword();
+       Authentication authentication = authenticate(email, password);
+        return null;
+    }
+
+    private Authentication authenticate(String email, String password){
         return null;
     }
 }
